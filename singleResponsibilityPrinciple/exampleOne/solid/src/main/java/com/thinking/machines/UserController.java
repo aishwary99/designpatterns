8d27967bb38f29da5fa0ac8796fe3f2719dee8b0
Aishwary Adwani <aishwaryadwani@192.168.1.8>
2024-02-04 12:27:21 +0530

    Added Code for Single Responsibility Principle

---
+++ singleResponsibilityPrinciple/exampleOne/solid/src/main/java/com/thinking/machines/UserController.java
@@ -0,0 +1,34 @@
+package com.thinking.machines;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+//Handles incoming JSON requests that work on User
+public class UserController {
+
+    private UserPersistenceService persistenceService = new UserPersistenceService();
+    
+    //Create a new user
+    public String createUser(String userJson) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        User user = mapper.readValue(userJson, User.class);
+
+        /**
+         * The validateUser functionality is moved to UserValidator.java
+         * instead of being handled in Controller
+         */
+
+        UserValidator validator = new UserValidator();
+        boolean valid = validator.validateUser(user);
+        
+        if(!valid) {
+            return "ERROR";
+        }
+
+        persistenceService.saveUser(user);
+        
+        return "SUCCESS";
+    } 
+
+}
